2be01f782c1d559e8066053a6747cf7b6e5dd633
Gabor Simon <adrianlorencic98@gmail.com>
2022-10-11 09:04:47 +0200

    Initial commit

---
+++ Assets/PlayfabManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PlayFab;
+using PlayFab.ClientModels;
+
+public class PlayfabManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Login();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Login()
+    {
+        var request = new LoginWithCustomIDRequest
+        {
+            CustomId = SystemInfo.deviceUniqueIdentifier,
+            CreateAccount = true
+        };
+
+        PlayFabClientAPI.LoginWithCustomID(request, OnSuccess, OnError);
+    }
+
+    void OnSuccess(LoginResult result)
+    {
+        print("Successfull login/create account");
+    }
+
+    void OnError(PlayFabError error)
+    {
+        print("Error while login/creating account");
+        print(error.GenerateErrorReport());
+    }
+}
